91659025516e804d2dd40b6124ecc54a13bf2081
fengjiran <fengjiran@foxmail.com>
2018-10-31 20:10:11 +0800

    add limits

---
+++ limits.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <climits>
+int main()
+{
+    using namespace std;
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+}
