d345888c54956969719d23a21a0ef6e883a58136
fengjiran <fengjiran@foxmail.com>
2018-11-05 23:35:36 +0800

    add

--- arrstruc.cpp
+++ arrstruc.cpp
@@ -13,4 +13,9 @@ int main()
         {
             {"Bambi", 0.5, 21.99},
             {"Godzilla", 2000, 565.99}};
+
+    cout << "The guests " << guests[0].name << " and " << guests[1].name
+         << "\nhave a combined volume of "
+         << guests[0].volume + guests[1].volume << " cubic feet.\n";
+    return 0;
 }
