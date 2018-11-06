2cf183f5c405b7708842ad1b8db7f721b0e65cef
fengjiran <fengjiran@foxmail.com>
2018-11-06 20:00:42 +0800

    pointer addition

---
+++ addpntrs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    double wages[3] = {10000.0, 20000.0, 30000.0};
+    short stacks[3] = {3, 2, 1};
+
+    double *pw = wages;
+    short *ps = &stacks[0];
+
+    cout << "pw = " << pw << ", *pw = " << *pw << endl;
+    pw = pw + 1;
+    cout << "add 1 to the pw pointer:\n";
+    cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << endl;
+}
